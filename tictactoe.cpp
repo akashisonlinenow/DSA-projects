614af9e4be2461f576a26d503f1ac68d899f9b4d
Akash Shridharan <akashshridharan2802@gmail.com>
2023-03-20 11:20:29 +0530

    finished

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -1,22 +1,16 @@
 #include <iostream>
 using namespace std;
 
-int main()
+char space[3][3] = {{'1', '2', '3'}, {'4', '5', '6'}, {'7', '8', '9'}};
+int row, column;
+char token = 'x';
+bool tie = false;
+string n1, n2;
+
+void functionOne()
 {
 
-    int space[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-
-    string n1, n2;
-    cout << "Enter the name of Player1 : \n";
-    getline(cin, n1);
-
-    cout << "Enter the name of Player2 : \n";
-    getline(cin, n2);
-
-    cout << n1 << " is Player1 so he/she will play first.\n";
-    cout << n2 << " is Player2 so he/she will play second.\n";
-
-    // creting structure and adding elements...
+    // creating structure and adding elements...
 
     cout << "    |      |   \n";
     cout << " " << space[0][0] << "  |  " << space[0][1] << "   |  " << space[0][2] << " \n";
@@ -27,4 +21,154 @@ int main()
     cout << "    |      |   \n";
     cout << " " << space[2][0] << "  |  " << space[2][1] << "   |  " << space[2][2] << " \n";
     cout << "    |      |   \n";
+}
+
+void functionTwo()
+{ // in this we will check for any invalid value
+    int digit;
+    if (token == 'x')
+    {
+        cout << n1 << " Please enter";
+        cin >> digit;
+    }
+    if (token == '0')
+    {
+        cout << n2 << " Please enter";
+        cin >> digit;
+    }
+
+    if (digit == 1)
+    {
+        row = 0;
+        column = 0;
+    }
+    if (digit == 2)
+    {
+        row = 0;
+        column = 1;
+    }
+    if (digit == 3)
+    {
+        row = 0;
+        column = 2;
+    }
+    if (digit == 4)
+    {
+        row = 1;
+        column = 0;
+    }
+    if (digit == 5)
+    {
+        row = 1;
+        column = 1;
+    }
+    if (digit == 6)
+    {
+        row = 1;
+        column = 2;
+    }
+    if (digit == 7)
+    {
+        row = 2;
+        column = 0;
+    }
+    if (digit == 8)
+    {
+        row = 2;
+        column = 1;
+    }
+    if (digit == 9)
+    {
+        row = 2;
+        column = 2;
+    }
+
+    else if (digit < 1 || digit > 9)
+    {
+        cout << "Invalid!!!"
+             << "\n";
+    }
+
+    if (token == 'x' && space[row][column] != 'x' && space[row][column] != '0')
+    {
+        space[row][column] = 'x';
+        token = '0';
+    }
+
+    else if (token == '0' && space[row][column] != 'x' && space[row][column] != '0')
+    {
+        space[row][column] = '0';
+        token = 'x';
+    }
+
+    else
+    {
+        cout << "There is no empty space!"
+             << "\n";
+        functionTwo();
+    }
+    functionOne();
+}
+
+bool functionThree()
+{
+    for (int i = 0; i < 3; i++)
+    { // condition to check if someone wins
+        if (space[i][0] == space[i][1] && space[i][0] == space[i][2] || space[0][i] == space[1][i] && space[0][i] == space[2][i])
+        {
+            return true;
+        }
+    }
+    if (space[0][0] == space[1][1] && space[1][1] == space[2][2] || space[0][2] == space[1][1] && space[1][1] == space[2][0])
+    {
+        return true;
+    }
+
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            if (space[i][j] != 'x' && space[i][j] != '0')
+            { // this condition is for whether the game is still ging on
+                return false;
+            }
+        }
+    }
+    tie = true;
+    return false;
+}
+
+int main()
+{
+    cout << "Enter the name of Player1 : \n";
+    getline(cin, n1);
+
+    cout << "Enter the name of Player2 : \n";
+    getline(cin, n2);
+
+    cout << n1 << " is Player1 so he/she will play first.\n";
+    cout << n2 << " is Player2 so he/she will play second.\n";
+
+    while (!functionThree())
+    {
+        functionOne();
+        functionTwo();
+        functionThree();
+    }
+
+    if (token == 'x' && tie == false)
+    {
+        cout << n2 << " wins!!!"
+             << "\n";
+    }
+    else if (token == '0' && tie == false)
+    {
+        cout << n1 << " wins!!!"
+             << "\n";
+    }
+    else
+    {
+        cout << "It's a draw!!"
+             << "\n";
+    }
 }
